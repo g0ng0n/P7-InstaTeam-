124d03c79b295f2da2b4649bc3251f5ef58801c6
g0ng0n <gonzalo.a.gisbert@gmail.com>
2017-04-29 14:20:16 -0300

    Step 8 - Create a DAO interface and implementation for each model class.

--- src/main/java/com/g0ng0n/instateam/dao/CollaboratorDaoImpl.java
+++ src/main/java/com/g0ng0n/instateam/dao/CollaboratorDaoImpl.java
@@ -1,7 +1,9 @@
 package com.g0ng0n.instateam.dao;
 
 import com.g0ng0n.instateam.model.Collaborator;
+import com.g0ng0n.instateam.model.Project;
 import com.g0ng0n.instateam.model.Role;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,16 +33,45 @@ public class CollaboratorDaoImpl implements CollaboratorDao {
 
     @Override
     public Collaborator findById(Long id) {
-        return null;
+
+        Session session = sessionFactory.openSession();
+
+        Collaborator collaborator = session.get(Collaborator.class, id);
+        // make sure that the collection of gifs is initialized before the session is close
+        Hibernate.initialize(collaborator.getRole());
+
+        session.close();
+        return collaborator;
     }
 
     @Override
     public void save(Collaborator collaborator) {
+        //Open session
+        Session session = sessionFactory.openSession();
 
+        // Begin trasanction
+        session.beginTransaction();
+
+        // save the project
+        session.saveOrUpdate(collaborator);
+
+        // commit the transaction
+        session.getTransaction().commit();
+
+        // close session
+        session.close();
     }
 
     @Override
     public void delete(Collaborator collaborator) {
+        //Open session
+        Session session = sessionFactory.openSession();
 
+        session.beginTransaction();
+        session.delete(collaborator);
+
+        session.getTransaction().commit();
+        // close session
+        session.close();
     }
 }
--- src/main/java/com/g0ng0n/instateam/dao/RoleDaoImpl.java
+++ src/main/java/com/g0ng0n/instateam/dao/RoleDaoImpl.java
@@ -1,6 +1,8 @@
 package com.g0ng0n.instateam.dao;
 
+import com.g0ng0n.instateam.model.Project;
 import com.g0ng0n.instateam.model.Role;
+import org.hibernate.Hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,16 +33,44 @@ public class RoleDaoImpl implements RoleDao {
 
     @Override
     public Role findById(Long id) {
-        return null;
+        Session session = sessionFactory.openSession();
+
+        Role role = session.get(Role.class, id);
+        // make sure that the collection of gifs is initialized before the session is close
+        Hibernate.initialize(role.getCollaborator());
+
+        session.close();
+        return role;
     }
 
     @Override
     public void save(Role role) {
+        //Open session
+        Session session = sessionFactory.openSession();
 
+        // Begin trasanction
+        session.beginTransaction();
+
+        // save the project
+        session.saveOrUpdate(role);
+
+        // commit the transaction
+        session.getTransaction().commit();
+
+        // close session
+        session.close();
     }
 
     @Override
     public void delete(Role role) {
+        //Open session
+        Session session = sessionFactory.openSession();
 
+        session.beginTransaction();
+        session.delete(role);
+
+        session.getTransaction().commit();
+        // close session
+        session.close();
     }
 }
