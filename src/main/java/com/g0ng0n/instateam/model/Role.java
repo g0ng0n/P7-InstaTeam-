767f1602bb8323f491360c107d83cef2a4ab92cd
g0ng0n <gonzalo.a.gisbert@gmail.com>
2017-04-29 13:46:42 -0300

    Create the Role model class, which represents the roles each project could contain, and that need to be filled

---
+++ src/main/java/com/g0ng0n/instateam/model/Role.java
@@ -0,0 +1,62 @@
+package com.g0ng0n.instateam.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+/**
+ * Created by g0ng0n.
+ */
+@Entity
+public class Role {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Pattern(regexp = "#[0-9a-fA-F]{6}")
+    private String name;
+
+
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role = (Role) o;
+
+        if (!id.equals(role.id)) return false;
+        return name.equals(role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + name.hashCode();
+        return result;
+    }
+}
